459c35165a244cca34d415000d576aa2f73a1ec3
Alexis Mineaud <alexis.mineaud@gmail.com>
2014-12-17 01:43:00 +0100

    Fix bower dependencies.

--- gulp/settings.js
+++ gulp/settings.js
@@ -1,8 +1,9 @@
 var settings = {
   bowerComponents: [
     'bower_components/angular/angular.min.js',
+    'bower_components/angularfire/dist/angularfire.min.js',
     'bower_components/firebase/firebase.js',
-    'bower_components/lodash/dist/lodash.min.js',
+    'bower_components/lodash/dist/lodash.min.js'
   ]
 };
 
